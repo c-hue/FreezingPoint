651a2a293f1f4c3215d7ea4dbd651b5e5debd653
jadethegemstone <143663485+jadethegemstone@users.noreply.github.com>
2026-05-06 14:24:29 -0700

    Adding Player Movement and Terrain

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,114 +4,59 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // --- Serialize Fields ------------------------------------------------------------------------
-    [Header("Movement")]
-    [SerializeField] float speed = 5f;
-    [SerializeField] float sprintSpeed = 7f;
-    [SerializeField] float turnSmoothTime = 0.1f;
-    [SerializeField] float turnSmoothVelocity;
-
-    [Header("Jump")]
-    [SerializeField] float gravity = -9.81f * 2;
-    [SerializeField] float jumpHeight = 3f;
-    [SerializeField] float fallMultiplier = 2.5f;
-    [SerializeField] float lowJumpMultiplier = 2f;
-
-    [Header("References")]
-    [SerializeField] Transform cameraTransform;
-    [SerializeField] CharacterController controller;
-    
-    private Vector3 velocity;
-    private Vector3 direction;
-    private bool isGrounded;
-
-
-    // --- Lifecycle ------------------------------------------------------------------------
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f * 2;
+    public float jumpHeight = 3f;
+ 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+ 
+    Vector3 velocity;
+ 
+    bool isGrounded;
+ 
+    // Update is called once per frame
     void Update()
     {
-        CheckGrounded();
-        ReadInput();
-        if (Input.GetKey(KeyCode.LeftShift)) Sprint();
-        else Walk();
-        HandleJump();
-        ApplyBetterJumpPhysics();
-        ApplyGravity();
-    }
-
-    // --- Input ------------------------------------------------------------------------
-    void ReadInput()
-    {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        // Camera
-        Vector3 camForward = cameraTransform.forward;
-        Vector3 camRight = cameraTransform.right; 
-        camForward.y = 0f;
-        camRight.y = 0f;
-        camForward.Normalize();
-        camRight.Normalize();
-
-        direction = (camRight * x + camForward * z).normalized;
-    }
-
-    // --- Movement ------------------------------------------------------------------------
-    void Walk()
-    {
-        if (direction.magnitude < 0.1f) return;
-        
-        RotateToMovement();
-        controller.Move(direction * speed * Time.deltaTime);
-    }
-
-    void Sprint()
-    {
-        if (direction.magnitude < 0.1f) return;
-        RotateToMovement();
-        controller.Move(direction * sprintSpeed * Time.deltaTime);
-    }
-
-    void RotateToMovement()
-    {
-        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-        transform.rotation = Quaternion.Euler(0f, angle, 0f);
-    }
-
-    // --- Ground Check ------------------------------------------------------------------------
-    void CheckGrounded()
-    {
-        isGrounded = controller.isGrounded;
+        //checking if we hit the ground to reset our falling velocity, otherwise we will fall faster the next time
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+ 
         if (isGrounded && velocity.y < 0)
         {
             velocity.y = -2f;
         }
-    }
 
-    // --- Jump ------------------------------------------------------------------------
-    void HandleJump()
-    {
+         if (Input.GetButton("Sprint"))
+        {
+            Debug.Log("Sprint");
+            speed = 24f;
+        } else
+        {
+            Debug.Log("Walking");
+            speed = 12f;
+        }
+ 
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+ 
+        //right is the red Axis, foward is the blue axis
+        Vector3 move = transform.right * x + transform.forward * z;
+ 
+        controller.Move(move * speed * Time.deltaTime);
+ 
+        //check if the player is on the ground so he can jump
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
+            //the equation for jumping
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
-    }
 
-    void ApplyBetterJumpPhysics()
-    {
-        if (velocity.y < 0)
-        {
-            velocity.y += gravity * (fallMultiplier - 1) * Time.deltaTime;
-        }
-        else if (velocity.y > 0 && !Input.GetButton("Jump"))
-        {
-            velocity.y += gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
-        }
-    }
-
-    void ApplyGravity()
-    {
+       
+ 
         velocity.y += gravity * Time.deltaTime;
+ 
         controller.Move(velocity * Time.deltaTime);
     }
 }
