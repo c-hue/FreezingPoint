b22b3583dd9fed860b02543eba44cad8c22021c3
c-hue <caitlyndhue@gmail.com>
2026-05-14 13:33:58 -0700

    changes

--- Assets/Scripts/Core/PauseGame.cs
+++ Assets/Scripts/Core/PauseGame.cs
@@ -30,6 +30,9 @@ public class PauseMenu : MonoBehaviour
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     public void ResumeGame()
@@ -37,6 +40,9 @@ public class PauseMenu : MonoBehaviour
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     public void RestartGame()
