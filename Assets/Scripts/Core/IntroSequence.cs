9dd62ef3430fc6900569f3e1312ba502c10835ba
c-hue <caitlyndhue@gmail.com>
2026-05-14 07:10:32 -0700

    moving stuff

--- Assets/Scripts/Core/IntroSequence.cs
+++ Assets/Scripts/Core/IntroSequence.cs
@@ -67,7 +67,7 @@ public class IntroSequence : MonoBehaviour
 
         yield return new WaitForSeconds(1f);
 
-        SceneManager.LoadScene("Gameplay 1");
+        SceneManager.LoadScene("Gameplay");
     }
 
     private IEnumerator Say(string text, string voiceName, int speakerIndex)
